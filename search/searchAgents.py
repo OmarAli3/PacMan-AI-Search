7a505becdf0b86e2cd0d6bd570687107fb8fe38c
Omar Ali Hassan <36759605+OmarAli3@users.noreply.github.com>
2021-01-10 03:29:12 +0200

    Adding Suboptimal Search

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -508,7 +508,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.ucs(problem)
 
 class AnyFoodSearchProblem(PositionSearchProblem):
     """
@@ -544,7 +544,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x,y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 def mazeDistance(point1, point2, gameState):
     """
